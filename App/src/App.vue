52b42fd77a3e04811f44c879d41a10413c942c0f
sumukh21-bit <sumukh.jagirdar@gmail.com>
2026-02-28 14:55:50 -0500

    Getting Started

---
+++ App/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import main from './components/main.vue'
+import 'bulma/css/bulma.css'
+</script>
+
+
+<template>
+<h1 class="is-size-4 has-text-primary">GradeCastify</h1>
+   <div class="columns is-centered">
+   <div class="has-text-centered  "> 
+  
+   <h1 class="is-size-4 has-text-white ">Login</h1>
+   <input class="input is-small" placeholder="User Name"></input>
+   <input class="input is-small" type="Password"placeholder="Password"></input>
+  
+   <button class="button is-white">Sign in</button>
+  
+  
+  </div></div>
+  
+  
+
+</template>
+
+
