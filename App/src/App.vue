151a113157ac880be6c4e1bb29596b1915d09ee7
sumukh21-bit <sumukh.jagirdar@gmail.com>
2026-03-10 22:11:12 -0400

    Made a simple Front end to start with!

--- App/src/App.vue
+++ App/src/App.vue
@@ -42,8 +42,10 @@ onUnmounted(() => {
 
 
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigatio"">
-   <div class="navbar-start is-size-5 has-text-primary">
+   <title>Grade Castify</title>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <img src="./logo.png" alt="logo" width="50">
+   <div class="navbar-start is-size-4 has-text-primary">
         GradeCastify
 
    </div>
@@ -53,7 +55,7 @@ onUnmounted(() => {
       <a class="is-size-6 navbar-item">
         Home
       </a>
-      <a class="is-size-6 navbar-item">
+      <a class="is-size-6 navbar-item" href="group_members.html">
         Group Members
       </a>
       <a class="is-size-6 navbar-item">
@@ -64,9 +66,13 @@ onUnmounted(() => {
 
 
   </nav>
+   <h1 class="has-text-centered is-size-4 has-text-success mt-6">Track and Predict Grades the right way</h1>
   
-  <div class="l-container py-6 mt-6 box column is-4">
-    <div class="columns is-centered" v-if="user">
+  <div class="columns is-centered mt-6  ">
+   
+    <div class="l-container  box column is-4 has-background-grey-darker  ">
+    
+    <div class="columns is-centered " v-if="user">
     <div class="my-3 py-5 has-text-centered">
       <h1 class="py-3 has-text-centered is-size-6">Hello {{ user.email }}</h1> 
       <button class="py-3 button is-small" @click="signOut">Sign out</button>
@@ -75,7 +81,7 @@ onUnmounted(() => {
 
   <div class="columns is-centered" v-else>
     <div class="my-3 py-5 has-text-centered">
-      <h1>Login</h1>
+      <h1 class="is-size-4 has-text-black">Login</h1>
       <input class="my-3 input is-small" v-model="email" placeholder="email" />
       <input class="my-3 input is-small" v-model="password" type="password" placeholder="password" />
       <button class="my-3 mx-2 button is-small" @click="signIn">Log in</button>
@@ -83,6 +89,7 @@ onUnmounted(() => {
     </div>
   </div>
 
-  </div>
+  </div></div>
+  
   
 </template>
