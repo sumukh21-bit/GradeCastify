65c8dc02034ecf1101145319dedb863d5388cf0f
sumukh21-bit <sumukh.jagirdar@gmail.com>
2026-03-10 20:51:16 -0400

    Made basic Login page

--- App/src/App.vue
+++ App/src/App.vue
@@ -42,10 +42,31 @@ onUnmounted(() => {
 
 
 <template>
-  <h1 class="pt-6 pb-1 has-text-centered is-size-4 has-text-primary">
-    GradeCastify
-  </h1>
-  <div class="columns is-centered" v-if="user">
+  <nav class="navbar" role="navigation" aria-label="main navigatio"">
+   <div class="navbar-start is-size-5 has-text-primary">
+        GradeCastify
+
+   </div>
+
+
+       <div class="navbar-end">
+      <a class="is-size-6 navbar-item">
+        Home
+      </a>
+      <a class="is-size-6 navbar-item">
+        Group Members
+      </a>
+      <a class="is-size-6 navbar-item">
+        Contact Us
+      </a>
+    </div>
+   
+
+
+  </nav>
+  
+  <div class="l-container py-6 mt-6 box column is-4">
+    <div class="columns is-centered" v-if="user">
     <div class="my-3 py-5 has-text-centered">
       <h1 class="py-3 has-text-centered is-size-6">Hello {{ user.email }}</h1> 
       <button class="py-3 button is-small" @click="signOut">Sign out</button>
@@ -54,10 +75,14 @@ onUnmounted(() => {
 
   <div class="columns is-centered" v-else>
     <div class="my-3 py-5 has-text-centered">
+      <h1>Login</h1>
       <input class="my-3 input is-small" v-model="email" placeholder="email" />
       <input class="my-3 input is-small" v-model="password" type="password" placeholder="password" />
       <button class="my-3 mx-2 button is-small" @click="signIn">Log in</button>
       <button class="my-3 mx-2 button is-small" @click="signUp">Sign up</button>
     </div>
   </div>
+
+  </div>
+  
 </template>
